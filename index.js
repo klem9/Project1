88c6339541c79fe287f51cd6ebb546f6fc4f7aa7
Klemense <klemenseloh@gmail.com>
2022-02-17 14:48:08 -0500

    xd

--- index.js
+++ index.js
@@ -7,22 +7,28 @@
 
 	today = yyyy + '-' + mm + '-' + dd;
 
-	fetch(`https://football-prediction-api.p.rapidapi.com/api/v2/predictions?market=classic&iso_date=${today}&federation=UEFA`, {
-		"method": "GET",
+	document.addEventListener("DOMContentLoaded", init())
+	
+	
+	function init () {
+		
+		fetch(`https://football-prediction-api.p.rapidapi.com/api/v2/predictions?market=classic&iso_date=${today}&federation=UEFA`,{
+		"method": "GET", 
 		"headers": {
 			"x-rapidapi-host": "football-prediction-api.p.rapidapi.com",
 			"x-rapidapi-key": "8ddd62d75dmshd960cc6a985507ep15c490jsn88d06d6139b4"
 		}
-	})
-	.then(response => response.json())
-	.then(data => {
-		data.data.forEach(game => {
-			renderGame(game)
 		})
-	})
-	.catch(err => {
-		console.error(err);
-	});
+		.then(response => response.json())
+		.then(data => {
+			data.data.forEach(game => {
+				renderGame(game)
+			})
+		})
+		.catch(err => {
+			console.error(err);
+		});
+	}
 
 	function renderGame(games){
 		//TODO: make a little slot for each game 
@@ -37,20 +43,23 @@
 		teamOneWin = games.odds[1]
 		teamTwoWin = games.odds[2]
 		onePlusDraw = games.odds['1X']
-		console.log(games.odds)
-		console.log(teamOneWin)
-		console.log(teamTwoWin)
-		console.log(onePlusDraw)
-
-
-	}
-
-	function contactForm(){
 
 	}
 
 	function betSizing(){
-		
+		sizingForm = document.getElementById("bet-form")
+		sizingInput = document.getElementById("sizing-input")
+		oddsInput = document.getElementById("odds-input")
+
+		bankroll = parseInt(sizingInput.value)
+		odds = parseInt(oddsInput.value)
+
+		if (odds <= 2){
+			betSize = bankroll * 0.02
+		} else {
+			betSize = bankroll * 0.015
+		}
+	
 	}
 
 	// competition name , start date
